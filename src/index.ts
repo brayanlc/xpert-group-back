21079e6002def43718ca4e9cba6f23dccf5b0cc9
brayan <brayanlc82@gmail.com>
2024-09-11 14:24:25 -0500

    Add MongoDB connection and catUser routes
    Integrated mongoose for MongoDB connection using environment variables for the connection URI. Added new catUser routes to the API for managing user-related functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import catRoutes from './routes/cat-routes';
 import catImagesRoutes from './routes/cat-image-routes';
+import catUserRoutes from './routes/cat-user-routes';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,6 +13,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api', catImagesRoutes);
 app.use('/api', catRoutes);
+app.use('/api', catUserRoutes);
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/catdb',
+  {},
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
