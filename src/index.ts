fbcfaa5d4909cde2ab95755c920c427340047bb0
brayan <brayanlc82@gmail.com>
2024-09-12 00:00:36 -0500

    Add CORS middleware
    Added CORS middleware to enable cross-origin resource sharing, allowing requests from different origins. This change improves the server's security and flexibility by handling cross-site HTTP requests appropriately. Additionally, reorganized dotenv configuration and added comments for better code readability.

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,25 @@ import catRoutes from './routes/cat-routes';
 import catImagesRoutes from './routes/cat-image-routes';
 import catUserRoutes from './routes/cat-user-routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
-dotenv.config();
-
+// Express
 const app = express();
+
+// Variables
 const PORT = process.env.PORT || 3000;
 
+// Settings
+dotenv.config();
 app.use(express.json());
+app.use(cors())
+
+// Routes
 app.use('/api', catImagesRoutes);
 app.use('/api', catRoutes);
 app.use('/api', catUserRoutes);
 
+// Database
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/catdb',
   {},
